ad5eda66f3b9434ddc05e90d69ecb2e08f2529f3
alex <alex_jm_1992@hotmail.com>
2025-09-04 08:59:39 +0000

    Intial commit

---
+++ HolaMundo/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+
+Console.WriteLine("This is the first line.");
+Console.WriteLine("This is the second line.");
