fecca062a36b9295c3c7278d935235d3a2504360
alex <alex_jm_1992@hotmail.com>
2025-09-09 07:39:25 +0000

    DESAFIO Ejercicio: Realización de una actividad de desafío con el ámbito de las variables

--- HolaMundo/Program.cs
+++ HolaMundo/Program.cs
@@ -3,3 +3,27 @@ Console.WriteLine("Hello, World!");
 
 Console.WriteLine("This is the first line.");
 Console.WriteLine("This is the second line.");
+
+//DESAFIO Ejercicio: Realización de una actividad de desafío con el ámbito de las variables
+
+int[] numbers = { 4, 8, 15, 16, 23, 42 };
+int total = 0;
+bool found = false;
+
+foreach (int number in numbers)
+{
+    total += number;
+
+    if (number == 42)
+    {
+       found = true;
+    }
+
+}
+
+if (found) 
+{
+    Console.WriteLine("Set contains 42");
+}
+
+Console.WriteLine($"Total: {total}");
